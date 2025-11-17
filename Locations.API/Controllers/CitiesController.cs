9fb6ab29c37575817c42e4e1de82113c2578c9d1
Ayşegül AKPINARLI <94296524+aayazakpinarli@users.noreply.github.com>
2025-11-17 11:45:39 +0300

    Location mivroservice is created

---
+++ Locations.API/Controllers/CitiesController.cs
@@ -0,0 +1,195 @@
+﻿#nullable disable
+using CORE.APP.Models;
+using Locations.APP.Features.Cities;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+//Generated from Custom Microservices Template.
+namespace Locations.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize(Roles = "Admin")]
+    public class CitiesController : ControllerBase
+    {
+        private readonly ILogger<CitiesController> _logger;
+        private readonly IMediator _mediator;
+
+        // Constructor: injects logger to log the errors to Kestrel Console or Output Window and mediator
+        public CitiesController(ILogger<CitiesController> logger, IMediator mediator)
+        {
+            _logger = logger;
+            _mediator = mediator;
+        }
+
+        // GET: api/Cities
+        [HttpGet]
+        [AllowAnonymous]
+        public async Task<IActionResult> Get()
+        {
+            try
+            {
+                // Send a query request to get query response
+                var response = await _mediator.Send(new CityQueryRequest());
+                // Convert the query response to a list
+                var list = await response.ToListAsync();
+                // If there are items, return them with 200 OK
+                if (list.Any())
+                    return Ok(list);
+                // If no items found, return 204 No Content
+                return NoContent();
+            }
+            catch (Exception exception)
+            {
+                // Log the exception
+                _logger.LogError("CitiesGet Exception: " + exception.Message);
+                // Return 500 Internal Server Error with an error command response with message
+                return StatusCode(StatusCodes.Status500InternalServerError, new CommandResponse(false, "An exception occured during CitiesGet.")); 
+            }
+        }
+
+        // GET: api/Cities/5
+        [HttpGet("{id}")]
+        [AllowAnonymous]
+        public async Task<IActionResult> Get(int id)
+        {
+            try
+            {
+                // Send a query request to get query response
+                var response = await _mediator.Send(new CityQueryRequest());
+                // Find the item with the given id
+                var item = await response.SingleOrDefaultAsync(r => r.Id == id);
+                // If item found, return it with 200 OK
+                if (item is not null)
+                    return Ok(item);
+                // If item not found, return 204 No Content
+                return NoContent();
+            }
+            catch (Exception exception)
+            {
+                // Log the exception
+                _logger.LogError("CitiesGetById Exception: " + exception.Message);
+                // Return 500 Internal Server Error with an error command response with message
+                return StatusCode(StatusCodes.Status500InternalServerError, new CommandResponse(false, "An exception occured during CitiesGetById.")); 
+            }
+        }
+
+		// POST: api/Cities
+        [HttpPost]
+        public async Task<IActionResult> Post(CityCreateRequest request)
+        {
+            try
+            {
+                // Check if the request model is valid through data annotations
+                if (ModelState.IsValid)
+                {
+                    // Send the create request
+                    var response = await _mediator.Send(request);
+                    // If creation is successful, return 200 OK with success command response
+                    if (response.IsSuccessful)
+                    {
+                        //return CreatedAtAction(nameof(Get), new { id = response.Id }, response);
+                        return Ok(response);
+                    }
+                    // If creation failed, add error command response message to model state
+                    ModelState.AddModelError("CitiesPost", response.Message);
+                }
+                // Return 400 Bad Request with all data annotation validation error messages and the error command response message if added seperated by |
+                return BadRequest(new CommandResponse(false, string.Join("|", ModelState.Values.SelectMany(v => v.Errors).Select(e => e.ErrorMessage))));
+            }
+            catch (Exception exception)
+            {
+                // Log the exception
+                _logger.LogError("CitiesPost Exception: " + exception.Message);
+                // Return 500 Internal Server Error with an error command response with message
+                return StatusCode(StatusCodes.Status500InternalServerError, new CommandResponse(false, "An exception occured during CitiesPost.")); 
+            }
+        }
+
+        // PUT: api/Cities
+        [HttpPut]
+        public async Task<IActionResult> Put(CityUpdateRequest request)
+        {
+            try
+            {
+                // Check if the request model is valid through data annotations
+                if (ModelState.IsValid)
+                {
+                    // Send the update request
+                    var response = await _mediator.Send(request);
+                    // If update is successful, return 200 OK with success command response
+                    if (response.IsSuccessful)
+                    {
+                        //return NoContent();
+                        return Ok(response);
+                    }
+                    // If update failed, add error command response message to model state
+                    ModelState.AddModelError("CitiesPut", response.Message);
+                }
+                // Return 400 Bad Request with all data annotation validation error messages and the error command response message if added seperated by |
+                return BadRequest(new CommandResponse(false, string.Join("|", ModelState.Values.SelectMany(v => v.Errors).Select(e => e.ErrorMessage))));
+            }
+            catch (Exception exception)
+            {
+                // Log the exception
+                _logger.LogError("CitiesPut Exception: " + exception.Message);
+                // Return 500 Internal Server Error with an error command response with message
+                return StatusCode(StatusCodes.Status500InternalServerError, new CommandResponse(false, "An exception occured during CitiesPut.")); 
+            }
+        }
+
+        // DELETE: api/Cities/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            try
+            {
+                // Send the delete request
+                var response = await _mediator.Send(new CityDeleteRequest() { Id = id });
+                // If delete is successful, return 200 OK with success command response
+                if (response.IsSuccessful)
+                {
+                    //return NoContent();
+                    return Ok(response);
+                }
+                // If delete failed, add error command response message to model state
+                ModelState.AddModelError("CitiesDelete", response.Message);
+                // Return 400 Bad Request with the error command response message
+                return BadRequest(new CommandResponse(false, string.Join("|", ModelState.Values.SelectMany(v => v.Errors).Select(e => e.ErrorMessage))));
+            }
+            catch (Exception exception)
+            {
+                // Log the exception
+                _logger.LogError("CitiesDelete Exception: " + exception.Message);
+                // Return 500 Internal Server Error with an error command response with message
+                return StatusCode(StatusCodes.Status500InternalServerError, new CommandResponse(false, "An exception occured during CitiesDelete.")); 
+            }
+        }
+
+
+
+        /// <summary>
+        /// Retrieves a list of cities that belong to the specified country.
+        /// </summary>
+        /// <param name="countryId">The unique identifier of the country whose cities are to be retrieved.</param>
+        /// <returns>
+        /// Returns <see cref="OkObjectResult"/> with the list of cities if any are found;
+        /// otherwise, returns <see cref="NoContentResult"/> if no cities exist for the given country.
+        /// </returns>
+        /// <remarks>
+        /// Example request: GET api/Cities/GetByCountryId/3
+        /// </remarks>
+        [HttpGet("[action]/{countryId}")]
+        [AllowAnonymous]
+        public async Task<IActionResult> GetByCountryId(int countryId)
+        {
+            var response = await _mediator.Send(new CityQueryRequest() { CountryId = countryId });
+            var list = await response.ToListAsync();
+            if (list.Any())
+                return Ok(list);
+            return NoContent();
+        }
+    }
+}
---
+++ Locations.APP/Features/Cities/CityDeleteHandler.cs
@@ -0,0 +1,31 @@
+﻿using CORE.APP.Models;
+using CORE.APP.Services;
+using Locations.APP.Domain;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Locations.APP.Features.Cities
+{
+    public class CityDeleteRequest : Request, IRequest<CommandResponse>
+    {
+    }
+
+    public class CityDeleteHandler : Service<City>, IRequestHandler<CityDeleteRequest, CommandResponse>
+    {
+        public CityDeleteHandler(DbContext db) : base(db)
+        {
+        }
+
+        public async Task<CommandResponse> Handle(CityDeleteRequest request, CancellationToken cancellationToken)
+        {
+            // isNoTracking is false for being tracked by EF Core to delete the entity
+            var entity = await Query(false).SingleOrDefaultAsync(city => city.Id == request.Id, cancellationToken);
+            if (entity is null)
+                return Error("City not found!");
+
+            Delete(entity);
+
+            return Success("City deleted successfully.", entity.Id);
+        }
+    }
+}
