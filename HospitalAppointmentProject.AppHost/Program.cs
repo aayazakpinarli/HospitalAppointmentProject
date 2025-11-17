9974d21776248538853cd6d7303cc14909707e28
Ayşegül AKPINARLI <94296524+aayazakpinarli@users.noreply.github.com>
2025-11-17 18:58:46 +0300

    Hospital service is created

--- HospitalAppointmentProject.AppHost/Program.cs
+++ HospitalAppointmentProject.AppHost/Program.cs
@@ -4,4 +4,6 @@ builder.AddProject<Projects.Users_API>("users-api");
 
 builder.AddProject<Projects.Locations_API>("locations-api");
 
+builder.AddProject<Projects.Hospitals_API>("hospitals-api");
+
 builder.Build().Run();
